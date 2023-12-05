736f1c299f4016362175fcf9c51d9e045f3e4602
Robert <robert89221@outlook.com>
2023-12-05 14:52:12 +0100

    Use CSS variable to control animation

--- src/App.js
+++ src/App.js
@@ -10,19 +10,28 @@ function MainContent()
   return (
     <p>
       <h1>Testing React</h1>
-      Name: {name}<br/>
-      Bool: {bool ? "It's true!" : "It's false"}<br/>
-      Number: {num}<br/>
+      <button onClick={ToggleAnim}>Toggle animation</button><br/><br/>
+      Variable 1: {name}<br/>
+      Variable 2: {bool ? "is True" : "is False"}<br/>
+      Variable 3: {num}<br/>
     </p>
   );
+}
+
+let spin = false;
+
+function ToggleAnim()
+{
+  spin = !spin;
+  document.documentElement.style.setProperty("--play-state", spin ? "running" : "paused");
 }
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={cupcake} className="App-logo" alt="Cupcake" />
-        <MainContent/>
+      <img src={cupcake} className="App-logo" alt="Cupcake"/>
+      <MainContent/>
       </header>
     </div>
   );
